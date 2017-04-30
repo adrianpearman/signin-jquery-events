aa76695f8aedfc8ab39c203480c7e499e3c86749
Adrian Pearman <adrianpearman12@gmail.com>
2017-04-30 19:31:45 -0400

    cd ..

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 $('.signin').click(function(){$('.modal').fadeIn();});
+
 $('button.close').click(function(){$('.modal').fadeOut();});
+
+$(".submit").on('click',function() {$("input").addClass("error")});
+
+// When the user puts their cursor in one of the input fields, remove the `.error` class from both input elements
+// $('input').on('click', function() {$('input').removeClass('error')});
+
+$('input').hover(function() {$('input').removeClass('error')});
